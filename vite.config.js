742362a2e39d9328d2062911a2c52d69ab61a117
Guilhem <gvinet@alter-watt.com>
2025-11-19 16:29:00 +0100

    [ADD] project init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/ADOM-break/', // <-- must match your repo name
+  plugins: [react()]
 })
